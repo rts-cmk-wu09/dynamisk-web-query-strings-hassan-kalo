63f31507c6b5ad2eaf7024627d481ca590ad748c
hassan-kalo <2430792@rts-365.dk>
2023-03-02 13:56:02 +0100

    add like btn

--- detination.js
+++ detination.js
@@ -1,31 +1,54 @@
-const wrapper = document.createElement('div')
-wrapper.classList.add('wrapper')
-document.body.append(wrapper)
 
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
+    const likeIcon = '♡'
+    const likedIcon = '♥️'
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("wrapper");
+    document.body.append(wrapper);
 
-fetch(`data/${id}.json`).then(response=>response.json())
-    .then(data=>{
-        const img = document.createElement('img')
-        img.src = data.image 
-        img.alt = data.title
-        const h2 = document.createElement('h2')
-        h2.innerText = data.destination
-        const h3 = document.createElement('h3')
-        h3.innerText = data.title
-        const h4 = document.createElement('h4')
-        h4.innerText = data.subtitle
-        const p = document.createElement('p')
-        p.innerText = data.text
-        const div = document.createElement('div')
-        div.classList.add('appartment')
-        const ul = document.createElement('ul')
-        data.facilities.forEach(element => {
-            const li = document.createElement('li')
-            li.innerText = element
-            ul.append(li)
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlParam_id = urlParams.get("id");
+  
+    fetch(`data/${urlParam_id}.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        const img = document.createElement("img");
+        img.src = data.image;
+        img.alt = data.title;
+        const h2 = document.createElement("h2");
+        h2.innerText = data.destination;
+        const h3 = document.createElement("h3");
+        h3.innerText = data.title;
+        const h4 = document.createElement("h4");
+        h4.innerText = data.subtitle;
+        const p = document.createElement("p");
+        p.innerText = data.text;
+        const div = document.createElement("div");
+        div.id = 'appartment' + urlParam_id;
+        div.dataset.favorite = localStorage.getItem('appartment' + urlParam_id) || likeBtn;
+        
+        div.classList.add("appartment");
+        const ul = document.createElement("ul");
+        data.facilities.forEach((element) => {
+          const li = document.createElement("li");
+          li.innerText = element;
+          ul.append(li);
         });
-        div.append(img,h2,h3,h4,p,ul)
-        wrapper.append(div) 
-    })
+        const likeBtn = document.createElement('span')
+        likeBtn.classList.add('like-btn')
+        div.append(img, h2, h3, h4, p, ul,likeBtn);
+        likeBtn.innerText = localStorage.getItem('appartment' + urlParam_id) || likeBtn;
+        likeBtn.addEventListener('click',likeBtnFunc)
+        wrapper.append(div);
+      });
+
+      function likeBtnFunc(){
+        console.log(this.parentNode.id)
+        if(localStorage.getItem(this.parentNode.id) == null || localStorage.getItem(this.parentNode.id) == likeIcon){
+          this.innerText = likedIcon
+          localStorage.setItem(this.parentNode.id,likedIcon)
+        }
+        else{
+          this.innerText = likeIcon
+          localStorage.setItem(this.parentNode.id,likeIcon)
+        }
+      }
--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 //.then(response=>response.json())
-const wrapper = document.createElement('div')
-wrapper.classList.add('wrapper')
-document.body.append(wrapper)
 
+const wrapper = document.createElement("div");
+wrapper.classList.add("wrapper");
+document.body.append(wrapper);
 
-fetch('data/destinations.json')
-  .then(response => response.json())
-  .then(data => {
-        for (let i = 0; i < data.destinations.length; i++) {
-            const img = document.createElement('img')
-            img.src = data.destinations[i].image 
-            img.alt = data.destinations[i].title
-            const h3 = document.createElement('h3')
-            const a = document.createElement('a')
-            a.innerText = data.destinations[i].title
-            a.href = `destination.html?id=${data.destinations[i].id}`
-            h3.append(a)
-            const h4 = document.createElement('h4')
-            h4.innerText = data.destinations[i].subtitle
-            const div = document.createElement('div')
-            div.classList.add('appartment')
-            div.append(img,h3,h4)
-            wrapper.append(div)
-        }}
-    )
-  .catch(error => console.error(error))
+fetch("data/destinations.json")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.destinations.length; i++) {
+      const img = document.createElement("img");
+      img.src = data.destinations[i].image;
+      img.alt = data.destinations[i].title;
+      const h3 = document.createElement("h3");
+      const a = document.createElement("a");
+      a.innerText = data.destinations[i].title;
+      a.href = `destination.html?id=${data.destinations[i].id}`;
+      h3.append(a);
+      const h4 = document.createElement("h4");
+      h4.innerText = data.destinations[i].subtitle;
+      const div = document.createElement("div");
+      div.id = 'appartment' + data.destinations[i].id;
+      div.dataset.favorite = localStorage.getItem('appartment' + data.destinations[i].id) || 0;
+      div.classList.add("appartment");
+      const likeBtn = document.createElement('span')
+      likeBtn.classList.add('like-btn')
+      likeBtn.classList.add('material-symbols-outlined')
+      likeBtn.innerText = 'favorite'
+      likeBtn.style.fontVariationSettings = `'FILL' ${localStorage.getItem('appartment' + data.destinations[i].id) || 0}, 'wght' 100, 'GRAD' 200, 'opsz' 48`;
+      //likeBtn.innerText = localStorage.getItem('appartment' + data.destinations[i].id) || likeIcon;
+      likeBtn.addEventListener('click',likeBtnFunc)
+      div.append(img, h3, h4,likeBtn);
+      wrapper.append(div);
+    }
+  })
+  .catch((error) => console.error(error));
+
+  function likeBtnFunc(){
+    
+    if(localStorage.getItem(this.parentNode.id) == null || localStorage.getItem(this.parentNode.id) == 0){
+      //this.innerText = likedIcon
+      localStorage.setItem(this.parentNode.id,1)
+      this.style.fontVariationSettings = "'FILL' 1, 'wght' 100, 'GRAD' 200, 'opsz' 48";
+    }
+    else{
+      //this.innerText = likeIcon
+      localStorage.setItem(this.parentNode.id,0)
+      this.style.fontVariationSettings = "'FILL' 0, 'wght' 100, 'GRAD' 200, 'opsz' 48";
+    }
+
+  }
 
